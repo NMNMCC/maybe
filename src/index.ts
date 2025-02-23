81d110c0a01e2a1c82e80adfefe69014224fecec
intzaaa <huangyinhaow@outlook.com>
2025-02-23 10:46:01 +0800

    return unique error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-const Error:
+export type Error =
   & symbol
-  & {
-    reason: string | undefined;
-  } = Object.assign(Symbol("error"), { reason: undefined });
-
-export type Error = typeof Error;
+  & Readonly<{
+    timestamp: number;
+  }>;
 
 export type MaybeError = [Error, undefined];
 export type MaybeSuccess<Success> = [undefined, Success];
@@ -14,9 +12,16 @@ export type Maybe<Success = void> = MaybeError | MaybeSuccess<Success>;
 export type IsError<T> = T extends MaybeError ? true : false;
 export type IsSuccess<T> = T extends MaybeSuccess<T> ? true : false;
 
-export const success = <T>(value: T): MaybeSuccess<T> => [undefined, value];
+export const succeed = <T>(value: T): MaybeSuccess<T> => [undefined, value];
 
-export const error = (description?: any): MaybeError => {
-  Error.reason = description === undefined ? description : String(description);
-  return [Error, undefined];
+export const fail = (description?: any): MaybeError => {
+  return [
+    Object.assign(Symbol(description), {
+      timestamp: Date.now(),
+    }),
+    undefined,
+  ];
 };
+
+export const success = succeed;
+export const error = fail;
