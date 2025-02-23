55c849687824fca22b757c33cfbafb7f053677a6
intzaaa <huangyinhaow@outlook.com>
2025-02-23 11:11:47 +0800

    allow any description

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,36 @@ export type IsSuccess<T> = T extends MaybeSuccess<T> ? true : false;
 
 export const succeed = <T>(value: T): MaybeSuccess<T> => [undefined, value];
 
-export const fail = (description?: string | number): MaybeError => {
+export const fail = (description: any): MaybeError => {
   return [
     Object.freeze(
-      Object.assign(Symbol(description), {
-        timestamp: Date.now(),
-      }),
+      Object.assign(
+        Symbol((() => {
+          switch (typeof description) {
+            case "string":
+            case "number":
+            case "undefined":
+              return description;
+            case "function":
+              return description.name;
+            case "bigint":
+              return description.toString();
+            case "symbol":
+              return description.description;
+            case "object":
+              try {
+                return JSON.stringify(description);
+              } catch {
+                return String(description);
+              }
+            default:
+              return String(description);
+          }
+        })()),
+        {
+          timestamp: Date.now(),
+        },
+      ),
     ),
     undefined,
   ];
---
+++ src/test.js
@@ -0,0 +1,3 @@
+import * as M from "../dist/index.js";
+
+console.log("test" === M.fail("test")[0].description);
